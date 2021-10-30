db97d14b4574406be60a416c4825498534697147
ShuaiShuai <1223086236@qq.com>
2021-10-30 17:45:19 +0800

    增加 20211030 1745

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,30 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+
+//把App启动类类当做一个springboot的自动化配置bean,并且能够开启整个工程类基于springboot的自动化配置
+@EnableAutoConfiguration
+@RestController
 public class App 
 {
+    @RequestMapping
+    public String home(){
+        return "hello world";
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+//        这行代码用于启动springboot项目
+        SpringApplication.run(App.class,args);
     }
 }
