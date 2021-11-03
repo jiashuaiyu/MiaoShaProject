e6d6c0600f05a49784326931642e665336c02c78
ShuaiShuai <1223086236@qq.com>
2021-11-03 21:10:12 +0800

    返回错误信息

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.example.controller;
 
 import org.example.controller.viewobject.UserVO;
+import org.example.error.BusinessException;
+import org.example.error.EmBusinessError;
 import org.example.response.CommonReturnType;
 import org.example.service.UserService;
 import org.example.service.model.UserModel;
@@ -22,18 +24,22 @@ public class UserController {
 
     @RequestMapping("/get")
     @ResponseBody
-    public CommonReturnType getUser(@RequestParam(name = "id")Integer id)
-    {
+    public CommonReturnType getUser(@RequestParam(name = "id")Integer id) throws BusinessException {
 //        调用service服务获取对应的id的用户对象并返回给前端
         UserModel userModel = userService.getUserById(id);
 
+//        若获取的用户信息不存在
+        if(userModel == null)
+        {
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
 
 //        将核心领域模型用户对象转化为可供用户使用的viewobject
         UserVO userVO = convertFormUserModel(userModel);
 
 //        返回通用对象
 //        通过CommonReturnType进行返回值错误归一化判断，并进行处理
-        return CommonReturnType.creat(userModel);
+        return CommonReturnType.creat(userVO);
     }
 
     private UserVO convertFormUserModel(UserModel userModel)
