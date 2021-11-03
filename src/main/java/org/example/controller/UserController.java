5e05c992d44ee460ffd067d4b6a17d8310380a41
ShuaiShuai <1223086236@qq.com>
2021-11-03 21:27:14 +0800

    异常处理01

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -8,10 +8,11 @@ import org.example.service.UserService;
 import org.example.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 //@Controller("user")用来指定controller的标记，使得它能被spring扫描到，这个controller标记的名字叫user
 //@RequestMapping("/user")  使得这个可以在浏览器上通过/user被访问到
@@ -31,6 +32,7 @@ public class UserController {
 //        若获取的用户信息不存在
         if(userModel == null)
         {
+//            错误被抛到了tomcat容器层
             throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
         }
 
@@ -51,5 +53,17 @@ public class UserController {
         UserVO userVO = new UserVO();
         BeanUtils.copyProperties(userModel, userVO);
         return userVO;
+    }
+
+//    定义exceptionHandler解决未被controller层吸收的exception
+//    @ExceptionHandler()需要设置发生什么样的错误之后就可以进入他的环节，此处设置的是Exception根类
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    public Object handlerException(HttpServletRequest request, Exception ex){
+
+        CommonReturnType commonReturnType = new CommonReturnType();
+        commonReturnType.setStatus("fail");
+        commonReturnType.setData(ex);
+        return commonReturnType;
     }
 }
